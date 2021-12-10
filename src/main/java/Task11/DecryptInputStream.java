fa0217a1e06de052ed57dafbc33aaca9c217044b
RusikVolc <ruclanvolchetskyy1990@gmail.com>
2021-12-10 19:55:52 +0200

    Added Task 11.

---
+++ src/main/java/Task11/DecryptInputStream.java
@@ -0,0 +1,19 @@
+package Task11;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DecryptInputStream extends FilterInputStream
+{
+    protected DecryptInputStream(InputStream in) {
+        super(in);
+    }
+
+    public int read(int z) throws IOException {
+        int c = super.read();
+        if(c == -1) return -1;
+        return (c^z) & 0xFF; /* Only use lowest 8 bits */
+    }
+}
+
---
+++ src/main/java/Task11/EncryptOutputStream.java
@@ -0,0 +1,18 @@
+package Task11;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class EncryptOutputStream extends FilterOutputStream
+{
+    public EncryptOutputStream(OutputStream o) {
+        super(o);
+    }
+
+    public void write(int c, int z) throws IOException {
+        super.write(c^z);
+    }
+
+}
+
